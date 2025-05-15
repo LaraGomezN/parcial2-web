279cdc1d452ff54a6c52b2c81732719ca720222f
LaraGomezN <n.lara@uniandes.edu.co>
2025-05-15 10:42:13 -0500

    metodo evaluacion

--- src/evaluacion/evaluacion.service.ts
+++ src/evaluacion/evaluacion.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { EvaluacionEntity } from './evaluacion.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class EvaluacionService {}
+export class EvaluacionService {
+
+    constructor(
+        @InjectRepository(EvaluacionEntity)
+        private readonly evaluacionRepository: Repository<EvaluacionEntity>
+    ){}
+    
+
+    async crearEvaluacion(evaluacion: EvaluacionEntity): Promise<EvaluacionEntity>{
+        if (evaluacion.profesor.nombre != 'mentor' && (evaluacion.proyecto.notaFinal < 0 || evaluacion.proyecto.notaFinal > 5))
+            throw new Error('No se puede crear la evaluacion')
+        return await this.evaluacionRepository.save(evaluacion)
+    }
+}
--- src/proyecto/proyecto.service.ts
+++ src/proyecto/proyecto.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ProyectoEntity } from './proyecto.entity';
+import { Repository } from 'typeorm';
+import { EstudianteEntity } from 'src/estudiante/estudiante.entity';
 
 @Injectable()
-export class ProyectoService {}
+export class ProyectoService {
+
+    constructor(
+        @InjectRepository(ProyectoEntity)
+        private readonly proyectoRepository: Repository<ProyectoEntity>
+    ){}
+
+    async crearProyecto(proyecto: ProyectoEntity): Promise<ProyectoEntity>{
+        if (proyecto.presupuesto <= 0 && proyecto.titulo.length <= 15)
+            throw new Error('No se puede crear el proyecto')
+        return await this.proyectoRepository.save(proyecto)
+    }
+
+    async findAllEstudiantes(id: number): Promise<EstudianteEntity>{
+        const proyecto = await this.proyectoRepository.findOne({where: {id}, relations: ['estudiantes']});
+        if (!proyecto) throw new Error('No se encuentra el proyecto')
+        return proyecto.estudiante;
+    }
+
+    
+
+
+}
