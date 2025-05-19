88dad7513bb78fffbe313ffab741c38c7c773187
LaraGomezN <111064261+LaraGomezN@users.noreply.github.com>
2025-05-18 22:04:27 -0500

    shared

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,26 @@ import { EstudianteModule } from './estudiante/estudiante.module';
 import { ProyectoModule } from './proyecto/proyecto.module';
 import { ProfesorModule } from './profesor/profesor.module';
 import { EvaluacionModule } from './evaluacion/evaluacion.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EstudianteEntity } from './estudiante/estudiante.entity';
+import { ProyectoEntity } from './proyecto/proyecto.entity';
+import { ProfesorEntity } from './profesor/profesor.entity';
+import { EvaluacionEntity } from './evaluacion/evaluacion.entity';
 
 @Module({
-  imports: [EstudianteModule, ProyectoModule, ProfesorModule, EvaluacionModule],
+  imports: [EstudianteModule, ProyectoModule, ProfesorModule, EvaluacionModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'parcialweb',
+      entities: [EstudianteEntity, ProyectoEntity, ProfesorEntity, EvaluacionEntity],
+      synchronize: true,
+      dropSchema: true
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/shared/testing-utils/typeorm-testing-config.ts
@@ -0,0 +1,17 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EstudianteEntity } from '../../estudiante/estudiante.entity';
+import { EvaluacionEntity } from 'src/evaluacion/evaluacion.entity';
+import { ProfesorEntity } from 'src/profesor/profesor.entity';
+import { ProyectoEntity } from 'src/proyecto/proyecto.entity';
+
+
+export const TypeOrmTestingConfig = () => [
+  TypeOrmModule.forRoot({
+    type: 'sqlite',
+    database: ':memory:',
+    dropSchema: true,
+    entities: [EstudianteEntity, EvaluacionEntity, ProfesorEntity, ProyectoEntity],
+    synchronize: true
+  }),
+  TypeOrmModule.forFeature([EstudianteEntity, EvaluacionEntity, ProfesorEntity, ProyectoEntity]),
+];
